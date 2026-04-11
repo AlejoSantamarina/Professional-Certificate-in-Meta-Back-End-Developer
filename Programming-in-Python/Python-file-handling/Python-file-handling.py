39672ab6384a36752579e35029ac6498634614cc
Alejo Santamarina <alejosantamarina@icloud.com>
2026-04-10 23:40:50 -0300

    Implement file creation if it does not exist

--- Programming-in-Python/Python-file-handling/Python-file-handling.py
+++ Programming-in-Python/Python-file-handling/Python-file-handling.py
@@ -1,3 +1,4 @@
+#opening a file
 try:
     with open('text.txt', mode = 'r') as file:
         data = file.readlines()
@@ -5,3 +6,8 @@ try:
         print(data)
 except FileNotFoundError as e:
     print(f"The file is not found. Type of error:{e}")
+
+#file creation
+
+with open('newfile.txt', 'w') as file:
+    file.write("This is a new file created.")
